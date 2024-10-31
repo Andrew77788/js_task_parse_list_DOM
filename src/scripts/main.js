4b733299bf335160a08a3af93017c03b8fad700f
Andrii Dotsulenko <58427426+Andrew77788@users.noreply.github.com>
2024-10-31 15:57:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const employees = Array.from(document.querySelectorAll('ul li'));
+
+employees.sort((a, b) => {
+  const salaryA = parseFloat(
+    a.getAttribute('data-salary').replace(/[^\d.-]/g, ''),
+  );
+  const salaryB = parseFloat(
+    b.getAttribute('data-salary').replace(/[^\d.-]/g, ''),
+  );
+
+  return salaryB - salaryA;
+});
+
+const employeeList = document.querySelector('ul');
+
+employeeList.innerHTML = '';
+
+// Додаємо відсортовані <li> елементи назад у <ul>
+employees.forEach((employee) => {
+  employeeList.appendChild(employee);
+});
